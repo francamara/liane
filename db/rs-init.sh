84501f9c9b3fa9df284ef239242b2a146dd4b572
wade98 <lolewade@gmail.com>
2022-10-14 18:05:18 -0300

    fix: docker replica sets now run correctly

--- db/rs-init.sh
+++ db/rs-init.sh
@@ -2,26 +2,22 @@
 
 mongosh <<EOF
 var config = {
-    "_id": "rs0",
-    "version": 1,
+    "_id": "mongo-set",
     "members": [
         {
+            "_id": 0,
+            "host": "mongo1:27017"
+        },
+        {
             "_id": 1,
-            "host": "mongo1:27017",
-            "priority": 3
+            "host": "mongo2:27017"
         },
         {
             "_id": 2,
-            "host": "mongo2:27017",
-            "priority": 2
-        },
-        {
-            "_id": 3,
-            "host": "mongo3:27017",
-            "priority": 1
+            "host": "mongo3:27017"
         }
     ]
 };
-rs.initiate(config, { force: true });
+rs.initiate(config);
 rs.status();
 EOF
