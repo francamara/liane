3dad1985ab238857fc8500b756e6a94f5c0680dd
Diego Carrera <dmcarrera19@gmail.com>
2022-10-27 21:10:48 -0300

    feat: navbar component added, responsive navbar for mobile screens added

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,18 +1,10 @@
-import { Button, Grid } from '@mui/material'
 import React from 'react'
-import LnTypo from '../LnTypo'
-import Link from 'next/link'
+import Navbar from '../../components/Navbar'
 import ImageSlider from '../../components/Carrousel'
 
+
 //TODO preguntar por orden de links, relevancia, SEO, etc
-const navLinkData = [
-  { item: 'Inicio', ref: '/' },
-  { item: '¿Qué es counselling?', ref: '#hero' },
-  { item: '¿Quién soy?', ref: '#aboutme' },
-  { item: '¿Cómo puedo ayudarte?', ref: '#mensaje' },
-  { item: 'El Pinche Mito', ref: '#bannerPodcast' },
-  { item: 'Contáctame', ref: '#footer' }
-]
+
 
 
 function Header() {
@@ -21,20 +13,8 @@ function Header() {
     <div>
       {/* div portada dibujito y nav */}
       <div id='portada' >
-        <div id='navbar'>
-          <Grid container direction={{ xs: 'column', sm: 'row' }} justifyContent='center' alignItems='center' spacing={{ xs: '0', sm: '24' }}>
-            {/*Se mapea el array con las etiquetas y rutas del navbar para generar cada link*/}
-            {navLinkData.map((navLink, i) => (
-              <Grid item sm='auto' key={i}>
-                <Button id='myButton'>
-                  <Link href={navLink.ref} >
-                    <LnTypo variant='h6'>{navLink.item}</LnTypo>
-                  </Link>
-                </Button>
-              </Grid>
-            ))}
-          </Grid>
-
+        <div className='navbarMobile'>
+          <Navbar />
         </div>
         <div id='carrouselContainer'>
           <ImageSlider />
---
+++ components/Navbar/index.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import { useState } from 'react'
+import DensitySmallIcon from '@mui/icons-material/DensitySmall'
+import { IconButton, Button, Grid } from '@mui/material/'
+import Link from 'next/link'
+import LnTypo from '../LnTypo'
+
+
+
+
+const navLinkData = [
+  { item: 'Inicio', ref: '/' },
+  { item: '¿Qué es counselling?', ref: '#hero' },
+  { item: '¿Quién soy?', ref: '#aboutme' },
+  { item: '¿Cómo puedo ayudarte?', ref: '#mensaje' },
+  { item: 'El Pinche Mito', ref: '#bannerPodcast' },
+  { item: 'Contáctame', ref: '#footer' }
+]
+
+
+
+
+export default function Navbar() {
+  const [isNavExpanded, setIsNavExpanded] = useState(false)
+
+  return (
+    <div className="nav">
+
+      <IconButton
+        className="navbarMobileButton"
+        onClick={() => {
+          setIsNavExpanded(!isNavExpanded)
+        }}
+      >
+        <DensitySmallIcon />
+      </IconButton>
+
+      <div
+        className={
+          isNavExpanded ? 'navbar expanded' : 'navbar'
+        }
+      >
+
+        <Grid container direction={{ xs: 'column', sm: 'row' }} justifyContent='center' alignItems='center' spacing={{ xs: '0', sm: '24' }}>
+          {/*Se mapea el array con las etiquetas y rutas del navbar para generar cada link*/}
+          {navLinkData.map((navLink, i) => (
+            <Grid item sm='auto' key={i}>
+              <Button id='myButton'>
+                <Link href={navLink.ref} >
+                  <LnTypo variant='h6' id='navbarText'>{navLink.item}</LnTypo>
+                </Link>
+              </Button>
+            </Grid>
+          ))}
+        </Grid>
+
+
+      </div>
+      {/* nav menu code... */}
+    </div>
+  )
+}
