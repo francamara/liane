ee691e549247b3186c896bd5fa57c942ce43e569
--replace-all <francamaradev@gmail.com>
2023-02-12 13:39:43 -0300

    feat: added redirects

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -2,8 +2,8 @@ import { Grid } from '@mui/material'
 import React from 'react'
 import LinkedInIcon from '@mui/icons-material/LinkedIn'
 import InstagramIcon from '@mui/icons-material/Instagram'
-import YouTubeIcon from '@mui/icons-material/YouTube'
-import PodcastsIcon from '@mui/icons-material/Podcasts'
+// import YouTubeIcon from '@mui/icons-material/YouTube'
+// import PodcastsIcon from '@mui/icons-material/Podcasts'
 import LnTypo from '../LnTypo'
 
 function Footer() {
@@ -14,7 +14,7 @@ function Footer() {
       <Grid container
         direction='row'
         justifyContent='center'
-        alignItems='flex-end'
+        alignItems='center'
         spacing={2}>
 
         <Grid item sm={10}>
@@ -25,10 +25,14 @@ function Footer() {
         <Grid item
           sm={2}
           textAlign='center'>
-          <LinkedInIcon color='primary' />
-          <InstagramIcon color='primary' />
-          <YouTubeIcon color='primary' />
-          <PodcastsIcon color='primary' />
+          <a href='https://www.linkedin.com/in/clr-liane-reinshagen-joho-phd-36118557' target="_blank" rel="noreferrer">
+            <LinkedInIcon color='primary' />
+          </a>
+          <a href='https://www.instagram.com/lianereinshagen/' target="_blank" rel="noreferrer">
+            <InstagramIcon color='primary' />
+          </a>
+          {/* <YouTubeIcon color='primary' /> */}
+          {/* <PodcastsIcon color='primary' /> */}
         </Grid>
 
 
